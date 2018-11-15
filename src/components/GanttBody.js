377a125e115ca96f5584d092e5e06b165d32b4ad
Alan Leite <alanleite@outlook.com>
2018-11-15 19:21:47 -0200

    feature: poc to add subtask

--- src/components/GanttBody.js
+++ src/components/GanttBody.js
@@ -1,142 +1,154 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 const GanttBodyContainer = styled.div`
-    overflow: hidden;
-    overflow-x: auto;
-    position: absolute;
-    left:700px;
-    top: 0;
-    right: 0;
-    bottom: -20px;
-    border: 1px solid lightgray;
-    > div {
-        height: 100%;
-    }
+  overflow: hidden;
+  overflow-x: auto;
+  position: absolute;
+  left: 700px;
+  top: 0;
+  right: 0;
+  bottom: -20px;
+  border: 1px solid lightgray;
+  > div {
+    height: 100%;
+  }
 `
 
 const GanttBodyBase = styled.div`
+  height: 100%;
+  .month {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    height: 30px;
+  }
+  .month .header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .grid {
+    display: flex;
     height: 100%;
-    .month {
-        display: flex;
-        flex-direction: row;
-        justify-content: flex-start;
-        height: 30px;
-      }
-      .month .header {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
+    flex-wrap: nowrap;
+  }
 
-      .grid {
-        display: flex;
-        height: 100%;
-        flex-wrap: nowrap;    
-      }
+  .grid .col {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    min-width: 30px;
+    height: 100%;
+    padding: 0;
+  }
+  .grid .col .header {
+    display: flex;
+    height: 30px;
+    padding: 8px 4px;
+    justify-content: center;
+    align-items: center;
+  }
 
-      .grid .col {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-around;
-        min-width: 30px;
-        height: 100%;
-        padding: 0;
-      }
-      .grid .col .header {
-        display: flex;
-        height: 30px;
-        padding: 8px 4px;
-        justify-content: center;
-        align-items: center;
-      }
-
-      .grid .col .last {
-        border: 1px solid red;
-      }
-      .grid .col .period {
-        flex-grow: 1;
-        border-right: 1px solid lightgray;
-      }
-      .col-filler {
-        flex-grow: 1;
-        background-color: white;
-      }
-      .content {
-        height: 100%;
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-      }
-`
-
-const GanttBodyContent = styled.div`
+  .grid .col .last {
+    border: 1px solid red;
+  }
+  .grid .col .period {
+    flex-grow: 1;
+    border-right: 1px solid lightgray;
+  }
+  .col-filler {
+    flex-grow: 1;
+    background-color: white;
+  }
+  .content {
     height: 100%;
     position: absolute;
     top: 0;
     bottom: 0;
     left: 0;
     right: 0;
-    .task {
-        position: absolute;
-        background-color: lightblue;
-        padding: 4px;
-    }
+  }
+`
+
+const GanttBodyContent = styled.div`
+  height: 100%;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`
+
+const taskStyles = {
+  task: css`
+    height: 30px;
+  `,
+  'task-top': css`
+    height: 14px;
+    top: ${p => p.top + 'px'};
+  `,
+  'task-bottom': css`
+    height: 14px;
+    top: ${p => p.top + 16 + 'px'};
+  `
+}
+
+const GanttTask = styled.div`
+  position: absolute;
+  background-color: lightblue;
+  width: ${p => p.width * 30 + 30 + 'px'};
+  left: ${p => p.left * 30 + 'px'};
+  ${p => taskStyles[p.type]};
 `
 
 class Gantt extends React.Component {
-    render() {
-        const columns = this.props.columns || []
-        const tasks = this.props.tasks
-        console.log(tasks)
-        return (
-            <GanttBodyContainer>
-            <div>
-
-                <GanttBodyBase>
-                    <div className='month'>
-                        {columns.month.map((month, i) => (
-                            <div 
-                                key={i} 
-                                className='header'
-                                style={{ minWidth: month.size * 30 }}>
-                                { month.name }
-                            </div>
-                        ))}
-                    </div>
-                    <div className='grid'>
-                        {columns.days.map((day, i) => (
-                            <div key={i} className='col'>
-                                <div className='header'>{ day }</div>
-                                <div className='period'></div>
-                            </div>
-                        ))}
-                        <div className='col-filler'/>
-                    </div>                    
-                </GanttBodyBase>
-
-                <GanttBodyContent>
-                    {tasks.map(task => (
-                        <div 
-                            className='task'
-                            style={{
-                                height: 30,
-                                width: (task.fx - task.ix) * 30 + 30,
-                                top: task.offsetY,
-                                left: (task.ix * 30),
-                                position: 'absolute'
-                            }}>
-                            <span>{ task.title }</span>
-                        </div>
-                    ))}
-                </GanttBodyContent>
-
+  render () {
+    const columns = this.props.columns || []
+    const tasks = this.props.tasks
+    return (
+      <GanttBodyContainer>
+        <div>
+          <GanttBodyBase>
+            <div className='month'>
+              {columns.month.map((month, i) => (
+                <div
+                  key={i}
+                  className='header'
+                  style={{ minWidth: month.size * 30 }}
+                >
+                  {month.name}
+                </div>
+              ))}
             </div>
-        </GanttBodyContainer>
-        )
-    }
+            <div className='grid'>
+              {columns.days.map((day, i) => (
+                <div key={i} className='col'>
+                  <div className='header'>{day}</div>
+                  <div className='period' />
+                </div>
+              ))}
+              <div className='col-filler' />
+            </div>
+          </GanttBodyBase>
+
+          <GanttBodyContent>
+            {tasks.map((task, i) => (
+              <GanttTask
+                key={i}
+                type={task.type}
+                top={task.offsetY}
+                width={task.fx - task.ix}
+                left={task.fx}
+                className={task.class}
+              />
+            ))}
+          </GanttBodyContent>
+        </div>
+      </GanttBodyContainer>
+    )
+  }
 }
 
 export default Gantt
--- src/core/helpers.js
+++ src/core/helpers.js
@@ -1,125 +1,136 @@
 const _months = {
-    0: 'Janeiro',
-    1: 'Fevereiro',
-    2: 'Março',
-    3: 'Abril',
-    4: 'Maio',
-    5: 'Junho',
-    6: 'Julho',
-    7: 'Agosto',
-    8: 'Setembro',
-    9: 'Outubro',
-    10: 'Novembro',
-    11: 'Dezembro'
-  }
-  
-  const vDate = (date) => {
-    let obj =  date
-    if(typeof obj === 'string'){
-      obj = new Date(date)
-    }
-    return {
-      day: obj.getDate(),
-      month: obj.getMonth(),
-      year: obj.getFullYear(),
-      wd: obj.getDay(),
-      date: obj,
-      sd: obj.toDateString()
-    }
-  }
-  
-  const addDays = function(date, days) {
-    var rDate = new Date(date.valueOf());
-    rDate.setDate(rDate.getDate() + days);
-    return rDate;
-  }
-  
-  const distinctGroups = (data) => {
-    return data.rows
-      .reduce((acc, r) => (
-      acc.includes(r.group)
-        ? acc
-        : [].concat(acc, r.group)
-      ), [])
-      .sort((a, b) => a > b ? 1 : -1)
-  }
-  
-  const makeColumns = (groups, data) => {
-    const to = vDate(data.to)
-    let current = vDate(data.from)
-    let index = 0
-    const m = {}
-    const d = []
-    const s = {}
-  
-    while(current && current.date <= to.date) {
-      if(!m[current.month]) {
-        m[current.month] = 1
-      } else {
-        m[current.month]++
-      }
-      s[current.sd] = index
-      d.push(current.day)
-      current = vDate(addDays(current.date, 1))
-      index++
-    }
+  0: 'Janeiro',
+  1: 'Fevereiro',
+  2: 'Março',
+  3: 'Abril',
+  4: 'Maio',
+  5: 'Junho',
+  6: 'Julho',
+  7: 'Agosto',
+  8: 'Setembro',
+  9: 'Outubro',
+  10: 'Novembro',
+  11: 'Dezembro'
+}
 
-    return {
-      month: Object.keys(m).map(key => ({
-        name: _months[key],
-        size: m[key]
-      })),
-      days: d,
-      sign: s
-    }
+const vDate = date => {
+  let obj = date
+  if (typeof obj === 'string') {
+    obj = new Date(date)
   }
-  
-  const makeData = (groups, columns, data) => {
-    const values = groups.map(group => {
-      const rows = data.rows.filter(p => p.group === group)
-      const rowData = rows
-        .map(p => 
-          Object.values(p.column).map(p => {
-            if(typeof p === 'string') {
-              return { name: p }
-            }
-            return p
-          })
-        )
-  
-      const tasks = rows
-        .map((p, i) => {
-          if(!p.tasks || p.tasks.length === 0) return []
-          return p.tasks.map(t => {
-            const from = vDate(t.from)
-            const to = vDate(t.to)
-            return Object.assign({}, t, {
-              ix: columns.sign[from.sd],
-              fx: columns.sign[to.sd],
-              y: i
-            })
-          })
+  return {
+    day: obj.getDate(),
+    month: obj.getMonth(),
+    year: obj.getFullYear(),
+    wd: obj.getDay(),
+    date: obj,
+    sd: obj.toDateString()
+  }
+}
+
+const addDays = function (date, days) {
+  var rDate = new Date(date.valueOf())
+  rDate.setDate(rDate.getDate() + days)
+  return rDate
+}
+
+const distinctGroups = data => {
+  return data.rows
+    .reduce(
+      (acc, r) => (acc.includes(r.group) ? acc : [].concat(acc, r.group)),
+      []
+    )
+    .sort((a, b) => (a > b ? 1 : -1))
+}
+
+const makeColumns = (groups, data) => {
+  const to = vDate(data.to)
+  let current = vDate(data.from)
+  let index = 0
+  const m = {}
+  const d = []
+  const s = {}
+
+  while (current && current.date <= to.date) {
+    if (!m[current.month]) {
+      m[current.month] = 1
+    } else {
+      m[current.month]++
+    }
+    s[current.sd] = index
+    d.push(current.day)
+    current = vDate(addDays(current.date, 1))
+    index++
+  }
+
+  return {
+    month: Object.keys(m).map(key => ({
+      name: _months[key],
+      size: m[key]
+    })),
+    days: d,
+    sign: s
+  }
+}
+
+const generateTaskClass = (taskIndex, parentTasksCount) => {
+  switch (parentTasksCount) {
+    case 1:
+      return 'task'
+    case 2:
+      return taskIndex === 0 ? 'task-top' : 'task-bottom'
+    default:
+      return 'task-hidden'
+  }
+}
+
+const makeData = (groups, columns, data) => {
+  const values = groups.map(group => {
+    const rows = data.rows.filter(p => p.group === group)
+
+    const rowData = rows.map(p =>
+      Object.values(p.column).map(s => {
+        if (typeof s === 'string') {
+          return { name: s }
+        }
+        return s
+      })
+    )
+
+    const tasks = rows.map((p, i) => {
+      if (!p.tasks || p.tasks.length === 0) return []
+      return p.tasks.map((t, it) => {
+        const from = vDate(t.from)
+        const to = vDate(t.to)
+        return Object.assign({}, t, {
+          ix: columns.sign[from.sd],
+          fx: columns.sign[to.sd],
+          y: i,
+          data: p.column,
+          type: generateTaskClass(it, p.tasks.length)
         })
-
-      return {
-        group,
-        data: rowData,
-        tasks
-      }
+      })
     })
-  
-    return values
-  }
-  
-  export function calculate (input) {
-    const groups = distinctGroups(input)
-    const columns = makeColumns(groups, input)
-    const data = makeData(groups, columns, input)
+
     return {
-      columns: {
-        month: columns.month,
-        days: columns.days,
-      },
-      data
+      group,
+      data: rowData,
+      tasks
     }
+  })
+
+  return values
+}
+
+export function calculate (input) {
+  const groups = distinctGroups(input)
+  const columns = makeColumns(groups, input)
+  const data = makeData(groups, columns, input)
+  return {
+    columns: {
+      month: columns.month,
+      days: columns.days
+    },
+    data
   }
+}
